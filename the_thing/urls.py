2662628bcbefd9c7c121535fcd7706eaaf88f253
Kevin Armstrong <kevin.t.armstrong@gmail.com>
2013-11-23 13:51:23 -0500

    Changed directions to event details, and added area info. Also some refactoring, and including page names in titles.

--- the_thing/urls.py
+++ the_thing/urls.py
@@ -4,8 +4,9 @@ from the_thing import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^directions/$', views.directions, name='directions'),
+    url(r'^area_info/$', views.area_info, name='area_info'),
     url(r'^fun_stuff/$', views.fun_stuff, name='fun_stuff'),
+    url(r'^event_details/$', views.event_details, name='event_details'),
     url(r'^rsvp/$', views.rsvp, name='rsvp'),
     url(r'^login/(?P<username>\w*)$', views.login, name='login'),
     
--- the_thing/views.py
+++ the_thing/views.py
@@ -2,36 +2,37 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
 from smtplib import SMTP_SSL
+import inspect, string
 
 from the_thing.models import Guest
 
 def index(request):
     all_guests = Guest.objects.all()
-    template = loader.get_template('the_thing/index.html')
-    context = RequestContext( request, {
-        'all_guests': all_guests,
-    })
+    current_function_name = _get_calling_method_name()
+    
+    return _render_template(request, current_function_name, { 'all_guests': all_guests })
+
+def _render_template(request, function_name, parameter_dictionary):
+    parameter_dictionary['page_title'] = _make_pretty_name(function_name)
+    template_name = 'the_thing/' + function_name + '.html'
+    template = loader.get_template(template_name)
+    context = RequestContext(request, parameter_dictionary)
     return HttpResponse(template.render(context))
 
-def will_attend(request, user_id):
-    guest = Guest.objects.get(id=user_id)
-    return HttpResponse("The user name is %s." % guest.name)
-    
+def _make_pretty_name(function_name):
+    pretty_function_name = string.capwords(function_name.replace('_', ' '))
+    return pretty_function_name
+
 def rsvp(request):
     username = request.session.get('user','')
     if username:
         guests = Guest.objects.filter(name__startswith=username)
         if guests:
             guest = guests[0]
-            template = loader.get_template('the_thing/rsvp.html')
-            context = RequestContext( request, {
-                'guest': guest,
-            })
-            return HttpResponse(template.render(context))
+            current_function_name = _get_calling_method_name()
+            return _render_template(request, current_function_name, { 'guest': guest })
     
-    template = loader.get_template('the_thing/login_required.html')
-    context = RequestContext( request )
-    return HttpResponse(template.render(context))
+    return _render_static_content(request, 'login_required')
 
 def login(request, username):
     if username:
@@ -54,30 +55,35 @@ def response(request, user_id):
     guest.save()
     
     #uncomment in production
-    #send_email_notification(guest)
+    #_send_email_notification(guest)
     
     if guest.name == "Tim Miller":
         return HttpResponseRedirect('http://www.youtube.com/watch?v=dQw4w9WgXcQ')
         
     return HttpResponseRedirect(reverse('index'))
 
-def directions(request):
-    template = loader.get_template('the_thing/directions.html')
-    context = RequestContext( request )
-    return HttpResponse(template.render(context))
+def area_info(request):
+	return _render_static_content(request, _get_calling_method_name())
+
+def event_details(request):
+	return _render_static_content(request, _get_calling_method_name())
     
 def fun_stuff(request):
-    template = loader.get_template('the_thing/fun_stuff.html')
-    context = RequestContext( request )
-    return HttpResponse(template.render(context))
+	return _render_static_content(request, _get_calling_method_name())
 
-def send_email_notification(guest):
+def _render_static_content(request, page_name):
+    return _render_template(request, page_name, {})
+
+def _get_calling_method_name():
+    return inspect.stack()[1][3]
+	
+def _send_email_notification(guest):
     smtp_server_name = "smtp.gmail.com"
     smtp_username = "theshortestnameavailable@gmail.com"
     smtp_password = "W3dding_"
     
     to_address = "kevin.t.armstrong@gmail.com"
-    message = make_message(guest)
+    message = _make_message(guest)
     smtp_server = SMTP_SSL(smtp_server_name)
     smtp_server.login(smtp_username, smtp_password)
     smtp_server.sendmail("from", to_address, message)
@@ -85,7 +91,7 @@ def send_email_notification(guest):
     
     return
     
-def make_message(guest):
+def _make_message(guest):
     response = "yes" if guest.will_attend else "no"
     message_body = guest.name + " just RSVPed " + response + ", with " + guest.number_of_guests + " guests."
     
